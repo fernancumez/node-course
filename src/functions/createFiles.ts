d283c839c35346f994a34741a066d589bf3d0e76
fernancumez <fernancumez@gmail.com>
2020-12-31 22:20:36 -0600

    updated create file functions

--- src/functions/createFiles.ts
+++ src/functions/createFiles.ts
@@ -1,7 +1,7 @@
 import { verifyFilesAndDirectories } from "../common/verifyFilesAndDirectories";
+import { createFilePromise } from "../common/createFiles/createFileWithPromise";
+import { createFileCallback } from "../common/createFiles/createFileWithCallback";
 import { createDirectory } from "../common/createDirectory";
-import { createFile } from "../common/createFileWithPromise";
-import { createFileCallback } from "../common/createFileWithCallback";
 import { outputNameDir } from "../constants";
 
 // Create file with promises
@@ -20,7 +20,8 @@ export const createFileWithPromise = async (
 
     // Check if the directory and file exists
     if (!isDirectoryExist) await createDirectory(outputNameDir);
-    if (!isFileExist) await createFile(outputNameDir, fileName, contentFile);
+    if (!isFileExist)
+      await createFilePromise(outputNameDir, fileName, contentFile);
   } catch (error) {
     console.error(error);
   }
@@ -33,20 +34,21 @@ export const createFileWithCallback = (
 ): void => {
   // Verify if the directory exists
   verifyFilesAndDirectories(`../functions/${outputNameDir}`)
-    .then((res) => {
-      if (res) {
+    .then((isDirectoryExist) => {
+      if (isDirectoryExist) {
         // Verify if the file exist
         verifyFilesAndDirectories(`../functions/${outputNameDir}/${fileName}`)
-          .then((res) => {
-            if (!res) createFileCallback(outputNameDir, fileName, contentFile);
+          .then((isFileExist) => {
+            if (!isFileExist)
+              createFileCallback(outputNameDir, fileName, contentFile);
           })
           .catch((err) => console.error(err));
       } else {
         // Verify if the file exist
         createDirectory(outputNameDir).then(() =>
           verifyFilesAndDirectories(`../functions/${outputNameDir}/${fileName}`)
-            .then((res) => {
-              if (!res)
+            .then((isFileExist) => {
+              if (!isFileExist)
                 createFileCallback(outputNameDir, fileName, contentFile);
             })
             .catch((err) => console.error(err))
